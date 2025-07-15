897f08d07c6407b89af3fb7a614a3f9dd259fc85
SAIKRUSHNAGOUDA <74015150+SAIKRUSHNAGOUDA@users.noreply.github.com>
2025-07-15 11:32:46 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
+// main.go
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"cloud-resource-mapper/azure"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <azure-subscription-id>")
+		os.Exit(1)
+	}
+
+	subscriptionID := os.Args[1]
+
+	fmt.Println("📡 Scanning Azure Resources...")
+	azure.ListAzureResources(subscriptionID)
+}
 
